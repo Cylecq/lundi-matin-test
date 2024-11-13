bed06e7d59598e4aca0aacf2b0f281ff8c7da7e5
Cyril <leclercq.cyril06@gmail.com>
2024-11-13 15:49:24 +0100

    [CLIENT]feat: fetch client in info page

--- client/src/pages/info/index.tsx
+++ client/src/pages/info/index.tsx
@@ -1,11 +1,55 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { useLocation } from "react-router-dom";
 import Box from "../../components/Box";
+import { ApiResponse, Client } from "../../lib/types";
 
 function Info() {
+  const location = useLocation();
+
+  const [client, setClient] = useState<Client>();
+  const [loading, setLoading] = useState(false);
+
+  const fetchClient = async (id: string) => {
+    const response = await axios.get<ApiResponse<Client>>(`${import.meta.env.VITE_SERVER_URL}/clients/${id}`, {
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem("token")}`,
+      },
+    });
+
+    setClient(response.data.datas);
+  };
+
+  useEffect(() => {
+    const id = location.pathname.split("/")[2];
+
+    setLoading(true);
+    fetchClient(id)
+      .catch(console.error)
+      .finally(() => setLoading(false));
+  }, []);
+
+  if (loading) {
+    return (
+      <Box>
+        <p>Chargement...</p>
+      </Box>
+    );
+  }
+
+  if (!client) {
+    return (
+      <Box>
+        <p>Aucun client trouvé</p>
+      </Box>
+    );
+  }
+
   return (
     <div className="flex flex-col gap-4">
       <Box>
         <div className="flex items-center justify-between">
-          <h1 className="text-2xl">Mathieu BOMPART</h1>
+          <h1 className="text-2xl">{client.nom}</h1>
           <button className="flex items-center gap-2 bg-secondary px-6 py-2 my-2 text-white rounded-md">
             <div className="w-4">
               <img src="/svg/gear.svg" alt="edit" />
@@ -20,33 +64,27 @@ function Info() {
         </div>
         <div className="flex justify-center">
           <table className="my-4">
-            <tr className="h-10">
-              <td className="border-r border-slate-200 px-4 text-right font-bold">
-                Prénom & NOM
-              </td>
-              <td className="px-4 text-left">Mathieu BOMPART</td>
-            </tr>
-            <tr className="h-10">
-              <td className="border-r border-slate-200 px-4 text-right font-bold">
-                Téléphone
-              </td>
-              <td className="px-4 text-left">01 23 45 67 89</td>
-            </tr>
-            <tr className="h-10">
-              <td className="border-r border-slate-200 px-4 text-right font-bold">
-                Email
-              </td>
-              <td className="px-4 text-left">mathieu@bompart.mb</td>
-            </tr>
-            <tr className="h-10">
-              <td className="border-r border-slate-200 px-4 text-right font-bold">
-                Adresse
-              </td>
-              <td className="px-4 text-left">
-                836 rue du Mas de Verchant <br />
-                34000 Montpellier
-              </td>
-            </tr>
+            <tbody>
+              <tr className="h-10">
+                <td className="border-r border-slate-200 px-4 text-right font-bold">Prénom & NOM</td>
+                <td className="px-4 text-left">{client.nom}</td>
+              </tr>
+              <tr className="h-10">
+                <td className="border-r border-slate-200 px-4 text-right font-bold">Téléphone</td>
+                <td className="px-4 text-left">{client.tel}</td>
+              </tr>
+              <tr className="h-10">
+                <td className="border-r border-slate-200 px-4 text-right font-bold">Email</td>
+                <td className="px-4 text-left">{client.email}</td>
+              </tr>
+              <tr className="h-10">
+                <td className="border-r border-slate-200 px-4 text-right font-bold">Adresse</td>
+                <td className="px-4 text-left">
+                  {client.adresse} <br />
+                  {client.code_postal} {client.ville}
+                </td>
+              </tr>
+            </tbody>
           </table>
         </div>
       </Box>
