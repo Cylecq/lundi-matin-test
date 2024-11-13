173adfb20c0ac07d5258869a8c14f37d31dcfb69
Cyril <leclercq.cyril06@gmail.com>
2024-11-13 16:37:03 +0100

    [CLIENT]feat: add link in nav bar

--- client/src/components/Layout.tsx
+++ client/src/components/Layout.tsx
@@ -1,11 +1,13 @@
-import { Outlet } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 
 function Layout() {
   return (
     <>
       <header className="bg-primary">
         <div className="w-[90%] mx-auto">
-          <p className="text-white py-2">Rechercher un contact</p>
+          <Link to="/">
+            <p className="text-white py-2">Rechercher un contact</p>
+          </Link>
         </div>
       </header>
       <main className="bg-slate-100 min-h-screen">
